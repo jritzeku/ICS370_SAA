fb7e32e78b8b7501f1b53ae5305987d44656b1e5
jritzeku <jritzeku@gmail.com>
2020-10-13 21:09:03 -0500

    added jumbotron with login and register button

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="jumbotron text-center">
 
-    <h1>Welcome to SAA </h1>
-    <p>This is an application for setting appointments among students and advisors.</p>
-
+        <h1>Welcome to SAA </h1>
+        <p>This is an application for setting appointments among students and advisors.</p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+            <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+        </p>
+    </div>
 @endsection
