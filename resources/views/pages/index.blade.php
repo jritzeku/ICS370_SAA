64adbbbdec92c74c8bff6d7a3b7b5df573e09e45
jritzeku <jritzeku@gmail.com>
2020-10-14 14:18:26 -0500

    fix issue with login and registration link

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,8 +5,8 @@
 
         <h1>Welcome to SAA </h1>
         <p>This is an application for setting appointments among students and advisors.</p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
-            <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+        <p><a class="btn btn-primary btn-lg" href= {{url('/login')}}  role="button">Login</a>
+            <a class="btn btn-success btn-lg"href= {{url('/register')}}  role="button">Register</a>
         </p>
     </div>
 @endsection
